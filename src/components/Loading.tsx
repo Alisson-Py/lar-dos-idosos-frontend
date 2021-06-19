45a6279b5e7c0497e929b6c6068dd98d493f3fae
Alisson-Py <alissilva1231@hotmail.com>
2021-06-18 23:10:15 -0300

    feat: udpate loading view

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import '../global/components/loading-view.css';
 
 export default function Loading() {
   return (
-    <div className="loading-icon">
-      <div className="square-1"></div>
-      <div className="square-2"></div>
-      <div className="square-3"></div>
+    <div className="loading-view">
+      <div className="loading-bar bar-1"/>
+      <div className="loading-bar bar-2"/>
+      <div className="loading-bar bar-3"/>
+      <div className="loading-bar bar-4"/>
+      <div className="loading-bar bar-5"/>
     </div>
   );
 }
